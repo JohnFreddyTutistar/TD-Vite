eeb6363c5ef848082a8ecf4fdd910be55d69eb64
JohnFreddyTutistar <johnfre.157@gmail.com>
2025-08-30 11:47:31 -0500

    renderizando app, aplicaciones de validaciones y optimizaciones

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -20,6 +20,78 @@ const initStore = () => {
   console.log('init store 🍏')
 }
 
+
+const loadStore = () => {
+    throw new Error('no implementado');
+}
+
+///// Funcionalidades básicas del TODO ///////////
+
+const getTodos = ( filter = Filter.All ) => {
+    switch( filter ){
+        case Filter.All:
+            return [...state.todos]
+
+        case Filter.Completed:
+            return state.todos.filter( todo => todo.done )
+
+        case Filter.Pending:
+            return state.todos.filter( todo => !todo.done )
+
+        default: 
+            throw new Error(`Option ${filter} is not valid`)
+    }
+}
+
+/**
+ * 
+ * @param {string} description 
+ */
+const addTodo = ( description ) => {
+    if( !description) throw new Error('description is required');
+
+    state.todos.push( new Todo(description))
+}
+
+/**
+ * 
+ * @param {String} todoId todoIdentifier
+ */
+const toggleTodo = ( todoId ) => {
+
+    state.todos = state.todos.map( todo => {
+        if(todo.id === todoId){
+            todo.done = !todo.done
+        }
+        return todo;
+    });
+
+}
+
+const deleteTodo = ( todoId ) => {
+    state.todos = state.filter(todo => todo.id !== todoId)
+}
+
+const deleteCompleted = () => {
+    state.todos = state.filter(todo => todo.done )
+}
+
+const setFilter = ( newFilter = Filter.All ) => {
+    state.filter = newFilter;
+}
+
+const getCurrentFilter = () => {
+    return state.filter
+}
+
 export default {
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
+    getTodos,
     initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
 }
--- src/todos/app.js
+++ src/todos/app.js
@@ -1,4 +1,10 @@
+import todoStore from "../store/todo.store";
 import html from "./app.html?raw";
+import { renderTodos } from "./uses-cases";
+
+const elementIds = {
+    TodoList: '.todo-list',
+}
 
 /**
  * 
@@ -6,11 +12,18 @@ import html from "./app.html?raw";
  */
 export const App = ( elementId ) => {
   
+    const displayTodos = () => {
+        const todos = todoStore.getTodos( todoStore.getCurrentFilter() )
+        renderTodos( elementIds.TodoList, todos )
+        
+    }
+
     // Cuando la funcion App se llama
     (() => {
         const app = document.createElement('div');
         app.innerHTML = html
         document.querySelector(elementId).append( app )
+        displayTodos();
     })()
 
 }
